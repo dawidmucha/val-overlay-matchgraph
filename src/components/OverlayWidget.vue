068b17a999a06c226777d6533194996d928de728
Dawid Mucha <dawidmucha98@gmail.com>
2024-07-14 20:17:39 +0200

    getting match history based on link (api atm down, can't test)

---
+++ src/components/OverlayWidget.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from 'axios'
+
+const matches = ref('')
+
+const route = useRoute()
+
+const getRecentMatches = async () => {
+  axios.get(`https://api.henrikdev.xyz/valorant/v3/matches/${route.params.region}/${route.params.name}/${route.params.tag}?api_key=${process.env.VUE_APP_VALAPI}`).then((res) =>
+    console.log(res.data)
+  )
+}
+
+onMounted(() => {
+  getRecentMatches()
+})
+</script>
+
+<template>
+  <div>
+    overlay {{ $route.params.name }} {{ $route.params.tag }} {{ $route.params.region }}
+    <hr />
+    {{ matches }}
+  </div>
+</template>
---
+++ src/router/router.vue
@@ -0,0 +1,21 @@
+<script>
+import { createWebHistory, createRouter } from 'vue-router'
+
+import HomeScreen from '@/components/HomeScreen.vue'
+import OverlayWidget from '@/components/OverlayWidget.vue'
+
+const routes = [
+  { path: '/', component: HomeScreen },
+  { path: '/:name/:tag/:region', component: OverlayWidget },
+  { path: '/:name', redirect: '/' },
+  { path: '/:name/:tag', redirect: '/' },
+  { path: '/:pathMatch(.*)*', redirect: '/' }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
+</script>
