a74fbb333efc5239391c4f637818e67a9a3dcab4
Dawid Mucha <dawidmucha98@gmail.com>
2024-07-15 20:18:07 +0200

    figuring out vue-chartjs :woozy:

--- src/components/OverlayWidget.vue
+++ src/components/OverlayWidget.vue
@@ -2,15 +2,26 @@
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import axios from 'axios'
+import { Line } from 'vue-chartjs'
 
 const matches = ref('')
+const data = ref({
+  labels: ['a', 'b', 'c'],
+  datasets: [
+    {
+      label: 'testing',
+      backgroundColor: '#f87979',
+      data: [30, 50, 24]
+    }
+  ]
+})
 
 const route = useRoute()
 
 const getRecentMatches = async () => {
-  axios.get(`https://api.henrikdev.xyz/valorant/v3/matches/${route.params.region}/${route.params.name}/${route.params.tag}?api_key=${process.env.VUE_APP_VALAPI}`).then((res) =>
-    console.log(res.data)
-  )
+  axios.get(`https://api.henrikdev.xyz/valorant/v1/mmr-history/${route.params.region}/${route.params.name}/${route.params.tag}?api_key=${process.env.VUE_APP_VALAPI}`).then((res) => {
+    matches.value = res.data 
+  })
 }
 
 onMounted(() => {
@@ -22,6 +33,14 @@ onMounted(() => {
   <div>
     overlay {{ $route.params.name }} {{ $route.params.tag }} {{ $route.params.region }}
     <hr />
-    {{ matches }}
+    <Line :data="data" />
+    <hr />
+    <div v-for="match in matches.data" :key="match" style="border: 1px solid black">
+      <div>{{ match.date_raw }}</div>
+      <div>{{ match.elo }}</div>
+      <div>{{ match.mmr_change_to_last_game }}</div>
+      <img :src="match.images.small" alt="rank icon" />
+    </div>
+    
   </div>
 </template>
